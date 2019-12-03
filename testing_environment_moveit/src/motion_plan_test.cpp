343ab3d47a81364767193fae6379df1140d247ed
Joel Meyer <joelmeyer2019@u.northwestern.edu>
2019-12-03 11:02:53 -0600

    motion_plan_test can now read current joint positions, has the correct starting joint positions

--- testing_environment_moveit/src/motion_plan_test.cpp
+++ testing_environment_moveit/src/motion_plan_test.cpp
@@ -9,11 +9,24 @@
 
 #include <moveit_visual_tools/moveit_visual_tools.h>
 
-// most recent testing_environment joint_state
+// global var: most recent testing_environment joint_state
 sensor_msgs::JointState curr_joint_state;
+std::vector<std::string> curr_joint_names;
+std::vector<double> curr_joint_positions;
 
+// combined joint_state callback
 void callback(const sensor_msgs::JointState& js){
   curr_joint_state = js;
+  ROS_WARN("curr_joint_state[0]: %f", curr_joint_state.position[0]);
+
+  std::vector<std::string> temp_string(std::begin(js.name), std::end(js.name));
+  std::vector<double> temp_positions(std::begin(js.position), std::end(js.position));
+  curr_joint_names = temp_string;
+  curr_joint_positions = temp_positions;
+
+  ROS_WARN("curr_joint_names[0]: %s", curr_joint_names[0].c_str());
+  ROS_WARN("curr_joint_positions[1]: %f", curr_joint_positions[1]);
+
 }
 
 int main(int argc, char** argv)
@@ -21,63 +34,42 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "motion_plan_test");
   ros::NodeHandle nh;
 
-  // wait for rviz to Initialize
+  // wait for rviz/arms to Initialize
   ros::Duration(3.0).sleep();
 
-  // testing arm_A planning group
-  // static const std::string PLANNING_GROUP = "wx200_arm_A/arm_controller";
-
-  // setup for controlling and planning for arm A
-  // moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
-
-  // setup for adding and removing collision objects in the world scene
-  // moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-
-  // using pointers for improved performance
-  // const robot_state::JointModelGroup* joint_model_group = move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
-
-  // test pose
-  // geometry_msgs::Pose target_pose1;
-  // target_pose1.orientation.w = 1.0;
-  // target_pose1.position.x = 0.1;
-  // target_pose1.position.y = 0.0;
-  // target_pose1.position.z = 0.1;
-  // move_group.setPoseTarget(target_pose1);
-
-  // compute and visualize plan
-  // moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-
-  // execute
-  // move_group.move();
-
+  // subscriber to combined joint_states
   ros::Subscriber curr_joint_sub = nh.subscribe("/testing_environment/joint_states", 1, callback);
 
+  // Look up robot description and construct robot model to use
   robot_model_loader::RobotModelLoader robot_model_loader("/testing_environment/robot_description");
   robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
   ROS_INFO("Model frame: %s", kinematic_model->getModelFrame().c_str());
 
+  // construct a robot state that maintains the configuration of the robot
   robot_state::RobotStatePtr kinematic_state(new robot_state::RobotState(kinematic_model));
-  // kinematic_state->setToDefaultValues();
-  // kinematic_state->setToRandomPositions();
-  ros::Duration(3.0).sleep();
-  kinematic_state->setVariableValues(curr_joint_state);
 
+  // main loop
+  ros::Rate loop_rate(50.0);
+  while (ros::ok()){
+    ros::spinOnce();
 
-  const robot_state::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("wx200_arm_A/arm_controller");
-  // const robot_state::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("testing_environment");
+    // kinematic_state->setVariablePosition("wx200_arm_B_waist", 10.0);
+    kinematic_state->setVariablePositions(curr_joint_names, curr_joint_positions);
+    const robot_state::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("testing_environment");
+    const std::vector<std::string>& joint_names = joint_model_group->getVariableNames();
 
+    // retrieve the current set of joint values tored in the state for the testing_environment
+    std::vector<double> joint_values;
+    kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
+    for (std::size_t i = 0; i < joint_names.size(); ++i)
+    {
+      ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
+    }
 
-  const std::vector<std::string>& joint_names = joint_model_group->getVariableNames();
-
-  std::vector<double> joint_values;
-  kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
-  for (std::size_t i = 0; i < joint_names.size(); ++i)
-  {
-    ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
+    loop_rate.sleep();
   }
 
-
-
-  ros::shutdown();
+  // ros::spin();
+  // ros::shutdown();
   return 0;
 }
