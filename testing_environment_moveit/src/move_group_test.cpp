b88abf228d11a7689d865e0c127b66856dde12f6
Joel Meyer <joelmeyer2019@u.northwestern.edu>
2019-12-07 23:24:17 -0600

    can now send simultaneous motion plans to both arms while using the testing_environment planning group

--- testing_environment_moveit/src/move_group_test.cpp
+++ testing_environment_moveit/src/move_group_test.cpp
@@ -17,104 +17,7 @@ int main(int argc, char** argv)
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
-  /********************* MoveGroupInterface test *****************************/
-
-  // static const std::string PLANNING_GROUP = "testing_environment";
-  static const std::string PLANNING_GROUP = "wx200_arm_A/arm_controller";
-  moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
-
-
-  // for adding and removing collision objects
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-
-  const robot_state::JointModelGroup* joint_model_group =
-    move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
-
-  // print out end-effector link for this group
-  ROS_INFO("End effector link: %s", move_group.getEndEffectorLink().c_str());
-
-  geometry_msgs::Pose test_pose1;
-  test_pose1.orientation.w = 1.0;
-  test_pose1.position.x = 0.0;
-  test_pose1.position.y = 0.0;
-  test_pose1.position.z = 0.45;
-  move_group.setPoseTarget(test_pose1);
-
-  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-
-  bool success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-
-  ROS_INFO("Visualizing test_pose 1 (pose goal) %s", success ? "" : "FAILED");
-
-  move_group.move();
-
-  ros::Duration(3.0).sleep(); // wait for plan to finish
-
-  /*********************** Path Constraint test *******************************/
-  ROS_WARN("Starting Path Constraint test...");
-
-  // create a path constraint
-  moveit_msgs::JointConstraint jc_wrist_angle;
-  jc_wrist_angle.joint_name = "wx200_arm_A_wrist_angle";
-  jc_wrist_angle.position = 0.0;
-  jc_wrist_angle.tolerance_above = 0.1;
-  jc_wrist_angle.tolerance_below = 0.1;
-  jc_wrist_angle.weight = 0.5; // denotes relative importance 0-1
-
-  moveit_msgs::JointConstraint jc_waist;
-  jc_waist.joint_name = "wx200_arm_A_waist";
-  jc_waist.position = 0.0;
-  jc_waist.tolerance_above = 0.1;
-  jc_waist.tolerance_below = 0.1;
-  jc_waist.weight = 0.4; // denotes relative importance 0-1
-
-  // set the path constraint for the move group
-  moveit_msgs::Constraints test_constraints;
-  test_constraints.joint_constraints.push_back(jc_wrist_angle);
-  test_constraints.joint_constraints.push_back(jc_waist);
-  move_group.setPathConstraints(test_constraints);
-
-  // set the start state to a new pose
-  robot_state::RobotState start_state(*move_group.getCurrentState());
-  geometry_msgs::Pose test_pose2;
-  test_pose2.orientation.w = 1.0;
-  test_pose2.position.x = 0.15;
-  test_pose2.position.y = 0.0;
-  test_pose2.position.z = 0.25;
-  // start_state.setFromIK(joint_model_group, test_pose2);
-  move_group.setStartState(start_state);
-
-  move_group.setPoseTarget(test_pose2);
-
-  move_group.setPlanningTime(10.0);
-  //
-  success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-  ROS_INFO("Visualizing plan 2 (constraints) %s", success ? "" : "FAILED");
-
-  move_group.move();
-
-  // move_group.clearPathConstraints();
-  ROS_WARN("Done!");
-
-
-  /********************* Second test ******************************************/
-  // moveit::core::RobotStatePtr current_state = move_group.getCurrentState();
-  //
-  // std::vector<double> joint_group_positions;
-  // current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
-  //
-  // // new pose in joint space
-  // joint_group_positions[0] = -1.0;
-  //
-  // move_group.setJointValueTarget(joint_group_positions);
-  //
-  // bool success;
-  // success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-  // ROS_INFO("Visualizing plan 2 (joint space goal) %s", success ? "" : "FAILED");
-  // //
-  // move_group.move();
-
-  /******* CollisionObject Test ***********************************************/
+  /************************ CollisionObject Test ******************************/
   // moveit_msgs::CollisionObject collision_object;
   // collision_object.header.frame_id = move_group.getPlanningFrame();
   //
@@ -142,6 +45,156 @@ int main(int argc, char** argv)
   //
   // planning_scene_interface.addCollisionObjects(collision_objects);
 
+
+  // /********************* MoveGroupInterface test *****************************/
+
+  static const std::string PLANNING_GROUP = "testing_environment";
+  moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
+
+  // for adding and removing collision objects
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+
+  const robot_state::JointModelGroup* joint_model_group =
+    move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+
+  // print out end-effector link for this group
+  // ROS_INFO("End effector link: %s", move_group.getEndEffectorLink().c_str());
+  // ROS_INFO("End effector: %s", move_group.getEndEffector().c_str());
+
+  geometry_msgs::Pose test_pose1;
+  test_pose1.orientation.w = 1.0;
+  test_pose1.position.x = 0.0;
+  test_pose1.position.y = 0.0;
+  test_pose1.position.z = 0.45;
+  move_group.setPoseTarget(test_pose1, "wx200_arm_A/ee_arm_link");
+
+  geometry_msgs::Pose test_pose2;
+  test_pose2.orientation.w = 1.0;
+  test_pose2.position.x = 0.0;
+  test_pose2.position.y = 0.22;
+  test_pose2.position.z = 0.45;
+  move_group.setPoseTarget(test_pose2, "wx200_arm_B/ee_arm_link");
+
+  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+
+  bool success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+  ROS_INFO("Visualizing test_pose 1 (pose goal) %s", success ? "" : "FAILED");
+
+  // move_group.asyncMove();
+  move_group.move();
+
+  ros::Duration(3.0).sleep(); // wait for plan to finish
+
+  /********************* Both move groups test ********************************/
+  // static const std::string PLANNING_GROUP_A = "wx200_arm_A/arm_controller";
+  // static const std::string PLANNING_GROUP_B = "wx200_arm_B/arm_controller";
+  //
+  // moveit::planning_interface::MoveGroupInterface move_group_A(PLANNING_GROUP_A);
+  // moveit::planning_interface::MoveGroupInterface move_group_B(PLANNING_GROUP_B);
+  //
+  // // for adding and removing collision objects
+  // moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+  //
+  // const robot_state::JointModelGroup* joint_model_group_A =
+  //   move_group_A.getCurrentState()->getJointModelGroup(PLANNING_GROUP_A);
+  // const robot_state::JointModelGroup* joint_model_group_B =
+  //   move_group_B.getCurrentState()->getJointModelGroup(PLANNING_GROUP_B);
+  //
+  // geometry_msgs::Pose test_pose1;
+  // test_pose1.orientation.w = 1.0;
+  // test_pose1.position.x = 0.2;
+  // test_pose1.position.y = 0.0;
+  // test_pose1.position.z = 0.1;
+  // move_group_A.setPoseTarget(test_pose1);
+  //
+  // geometry_msgs::Pose test_pose2;
+  // test_pose2.orientation.w = 1.0;
+  // test_pose2.position.x = 0.2;
+  // test_pose2.position.y = 0.22;
+  // test_pose2.position.z = 0.45;
+  // move_group_B.setPoseTarget(test_pose2);
+  //
+  // moveit::planning_interface::MoveGroupInterface::Plan my_plan_A;
+  // moveit::planning_interface::MoveGroupInterface::Plan my_plan_B;
+  //
+  // bool success_A = (move_group_A.plan(my_plan_A) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  // bool success_B = (move_group_B.plan(my_plan_B) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  //
+  // ROS_INFO("Visualizing test_pose 1 (pose goal) %s", success_A ? "" : "FAILED");
+  // ROS_INFO("Visualizing test_pose 2 (pose goal) %s", success_B ? "" : "FAILED");
+  //
+  // // move_group_A.setPlanningTime(10.0);
+  // // move_group_B.setPlanningTime(10.0);
+  //
+  // move_group_A.move();
+  // move_group_B.move();
+  //
+  // ros::Duration(3.0).sleep(); // wait for plan to finish
+
+  /*********************** Path Constraint test *******************************/
+  // ROS_WARN("Starting Path Constraint test...");
+  //
+  // // create a path constraint
+  // moveit_msgs::JointConstraint jc_wrist_angle;
+  // jc_wrist_angle.joint_name = "wx200_arm_A_wrist_angle";
+  // jc_wrist_angle.position = 0.0;
+  // jc_wrist_angle.tolerance_above = 0.1;
+  // jc_wrist_angle.tolerance_below = 0.1;
+  // jc_wrist_angle.weight = 0.5; // denotes relative importance 0-1
+  //
+  // moveit_msgs::JointConstraint jc_waist;
+  // jc_waist.joint_name = "wx200_arm_A_waist";
+  // jc_waist.position = 0.0;
+  // jc_waist.tolerance_above = 0.1;
+  // jc_waist.tolerance_below = 0.1;
+  // jc_waist.weight = 0.4; // denotes relative importance 0-1
+  //
+  // // set the path constraint for the move group
+  // moveit_msgs::Constraints test_constraints;
+  // test_constraints.joint_constraints.push_back(jc_wrist_angle);
+  // test_constraints.joint_constraints.push_back(jc_waist);
+  // move_group.setPathConstraints(test_constraints);
+  //
+  // // set the start state to a new pose
+  // robot_state::RobotState start_state(*move_group.getCurrentState());
+  // geometry_msgs::Pose test_pose2;
+  // test_pose2.orientation.w = 1.0;
+  // test_pose2.position.x = 0.15;
+  // test_pose2.position.y = 0.0;
+  // test_pose2.position.z = 0.25;
+  // // start_state.setFromIK(joint_model_group, test_pose2);
+  // move_group.setStartState(start_state);
+  //
+  // move_group.setPoseTarget(test_pose2);
+  //
+  // move_group.setPlanningTime(10.0);
+  // //
+  // success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  // ROS_INFO("Visualizing plan 2 (constraints) %s", success ? "" : "FAILED");
+  //
+  // move_group.move();
+  //
+  // ROS_WARN("Done!");
+  // ROS_WARN("Clearing path constraints...");
+  // move_group.clearPathConstraints();
+
+  /********************* Joint space test ******************************************/
+  // moveit::core::RobotStatePtr current_state = move_group.getCurrentState();
+  //
+  // std::vector<double> joint_group_positions;
+  // current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
+  //
+  // // new pose in joint space
+  // joint_group_positions[0] = -1.0;
+  //
+  // move_group.setJointValueTarget(joint_group_positions);
+  //
+  // bool success;
+  // success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  // ROS_INFO("Visualizing plan 2 (joint space goal) %s", success ? "" : "FAILED");
+  // //
+  // move_group.move();
 
 
   /******* Cartesian Path Test ***********************************************/
